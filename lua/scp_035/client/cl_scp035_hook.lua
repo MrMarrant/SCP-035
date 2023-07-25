4609a892fe648cc6904b9d4d973b403ff68fdbff
MrMarant <laughymclaugher@gmail.com>
2023-07-25 22:52:17 +0200

    [FEATURE] Add Overlay

--- lua/scp_035/client/cl_scp035_hook.lua
+++ lua/scp_035/client/cl_scp035_hook.lua
@@ -16,6 +16,19 @@
 
 if SERVER then return end
 
+local tab = {
+	["$pp_colour_addr"] = 0,
+	["$pp_colour_addg"] = 0,
+	["$pp_colour_addb"] = 0,
+	["$pp_colour_brightness"] = 0.1,
+	["$pp_colour_contrast"] = 1,
+	["$pp_colour_colour"] = 0,
+	["$pp_colour_mulr"] = 0,
+	["$pp_colour_mulg"] = 0,
+	["$pp_colour_mulb"] = 0
+}
+local maxRange = 200
+
 hook.Add("PreDrawHalos", "PreDrawHalos.AegisHaloSWEP", function()
 	local ply = LocalPlayer()
 	if (!IsValid( ply )) then return end
@@ -31,8 +44,42 @@ hook.Add( "OnScreenSizeChanged", "OnScreenSizeChanged.SCP035_ScreenSize", functi
 end )
 
 hook.Add( "RenderScreenspaceEffects", "RenderScreenspaceEffects.SCP035_WearMask", function()
-
     if (LocalPlayer().SCP035_IsWear) then
-        DrawMaterialOverlay( "effects/invuln_overlay_red", -0.06)
+        local allPlayers = player.GetAll() -- TODO changer ça putain mdr
+        local angle = EyeAngles()
+    
+        angle = Angle( 0, angle.y, 0 )
+        angle:RotateAroundAxis( angle:Up(), -90 )
+        angle:RotateAroundAxis( angle:Forward(), 90 )
+
+        DrawColorModify( tab )
+        DrawSobel( 0.1 )
+        cam.Start3D()
+        render.SetStencilEnable( true )
+        render.SetStencilWriteMask( 1 )
+        render.SetStencilTestMask( 1 )
+        render.SetStencilReferenceValue( 1 )
+        render.SetStencilFailOperation( STENCIL_KEEP )
+        render.SetStencilZFailOperation( STENCIL_KEEP )
+
+        for key, ent in ipairs(allPlayers) do
+            render.ClearStencil()
+            local range = ent:GetPos():Distance( EyePos() )
+            local transparency = 1 - math.Clamp( ( range - 300 ) / (maxRange/2), 0, 1 )
+
+            render.SetBlend( math.min( transparency, 0.99 ) )
+            render.SetStencilCompareFunction( STENCIL_ALWAYS )
+            render.SetStencilPassOperation( STENCIL_REPLACE )
+            ent:DrawModel()
+            render.SetStencilCompareFunction( STENCIL_EQUAL )
+            render.SetStencilPassOperation( STENCIL_KEEP )
+
+            cam.Start2D()
+                surface.SetDrawColor( Color(0,0,0, 250 * transparency * 0.2) )
+                surface.DrawRect( 0, 0, AEGIS_CONFIG.ScrW, AEGIS_CONFIG.ScrH )
+            cam.End2D()
+        end
+        render.SetStencilEnable( false )
+    cam.End3D()
     end
 end )
